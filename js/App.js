71b20adc5e7db763c041f6a5decfc1a47d18a520
DukeJeans <nicholasjeans@gmail.com>
2020-03-15 22:32:37 -0400

    App component & ESLint plugins
    Signed-off-by: DukeJeans <nicholasjeans@gmail.com>

--- js/App.js
+++ js/App.js
@@ -3,24 +3,14 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Tux",
-      animal: "Dog",
-      breed: "Labradoodle"
-    }),
-    React.createElement(Pet, {
-      name: "Raisin",
-      animal: "Dog",
-      breed: "Cockapoo"
-    }),
-    React.createElement(Pet, {
-      name: "Sporty",
-      animal: "Dog",
-      breed: "Golden Retriever"
-    })
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Tux" animal="Dog" breed="Labradoodle" />
+      <Pet name="Raisin" animal="Dog" breed="Cockapoo" />
+      <Pet name="Sporty" animal="Dog" breed="Golden Retriever" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
