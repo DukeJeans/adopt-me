2cd01809eb7d27401611a48ca5f20702d9605a45
DukeJeans <nicholasjeans@gmail.com>
2020-03-15 22:50:43 -0400

    Create SearchParams component
    Signed-off-by: DukeJeans <nicholasjeans@gmail.com>

--- js/App.js
+++ js/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Tux" animal="Dog" breed="Labradoodle" />
-      <Pet name="Raisin" animal="Dog" breed="Cockapoo" />
-      <Pet name="Sporty" animal="Dog" breed="Golden Retriever" />
+      <SearchParams />
     </div>
   );
 };
